5e0d9b0c811c68bad1756ad4d55185ba3ef22c4e
Shahid Tariq <shahidtariqnnn@gmail.com>
2018-02-01 20:15:23 +0530

    xml parsing on clinical study xml

--- clinicalSearch/models.py
+++ clinicalSearch/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class ClinicalStudy(models.Model):
+    org_study_id = models.CharField(max_length=100)
+    nct_id = models.CharField(max_length=100)
+    official_title = models.CharField(max_length=100)
+    lead_sponsor_agency = models.CharField(max_length=100)
+    lead_sponsor_agency_class = models.CharField(max_length=100)
+    source = models.CharField(max_length=500)
+    brief_summary = models.TextField()
+    detail_description = models.TextField()
+    overall_status = models.CharField(max_length=100)
+    start_date = models.CharField(max_length=100) #string date may be parsed before inserting
+    completion_date = models.CharField(max_length=100) #string date may be parsed before inserting
+    study_type = models.CharField(max_length=100)
+    no_of_arms = models.IntegerField(default=0)
+
+
+class Outcome(models.Model):
+    clinicalStudyId = models.ForeignKey(ClinicalStudy, on_delete=models.CASCADE)
+    outcome_type = models.CharField(max_length=200)  # can be primary or secondary
+    measure = models.TextField()
+    timeFrame = models.TextField()
+    description = models.TextField()
+
+
+class Condition(models.Model):
+    clinicalStudyId = models.ForeignKey(ClinicalStudy, on_delete=models.CASCADE)
+    condition_name = models.CharField(max_length=200)
--- clinicalSearch/views.py
+++ clinicalSearch/views.py
@@ -1,13 +1,12 @@
 from django.http import HttpResponse
 from django.views import View
-from django.template import loader
-from django.contrib.staticfiles.templatetags.staticfiles import static
-from django.contrib.staticfiles.storage import staticfiles_storage
-from xml.dom import minidom
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 from django.template import Context, loader
 import xml.etree.ElementTree as ET
-import csv
+import datetime
+from . models import ClinicalStudy,Condition,Outcome
+
 import os
 
 class Index(View):
@@ -27,17 +26,62 @@ class Index(View):
         cites_list = []
         pmid_list = []
 
-        for elt in tree.iter():
-            print("%s: '%s'" % (elt.tag, elt.text))
+        # for elt in tree.iter():
+        #    print("%s: '%s'" % (elt.tag, elt.text))
 
-        for data in root.findall('reference'):
-            citation = data.find('citation').text
-            pmid = data.find('PMID').text
-            cites_list.append(citation)
-            pmid_list.append(pmid)
 
-        context = {
-            'citation': cites_list,
-            'pmid': pmid_list,
-        }
+        #create model instance
+        clinicalSerachModel=ClinicalStudy()
+
+
+        #insert org_id
+        for data in root.findall('id_info'):
+            clinicalSerachModel.org_study_id=data.find('org_study_id').text
+            clinicalSerachModel.nct_id = data.find('nct_id').text
+
+        #insert official title,source
+        official_title = root.find('official_title').text
+        clinicalSerachModel.official_title=official_title
+        clinicalSerachModel.source = root.find('source').text
+        clinicalSerachModel.overall_status = root.find('overall_status').text
+        clinicalSerachModel.start_date = root.find('start_date').text
+        clinicalSerachModel.completion_date = root.find('completion_date')
+
+        # clinicalSerachModel.start_date = datetime.datetime.strptime(root.find('start_date').text, "%B %Y").date()
+        # clinicalSerachModel.completion_date = datetime.datetime.strptime(root.find('completion_date').text, "%B %Y").date()
+
+
+
+        # insert sponsor
+        for data in root.findall('sponsors'):
+            for x in data.findall('lead_sponsor'):
+                agency = x.find('agency').text
+                agency_class=x.find('agency_class').text
+                clinicalSerachModel.lead_sponsor_agency = agency
+                clinicalSerachModel.lead_sponsor_agency_class = agency_class
+
+        # insert brief textblock
+        for data in root.findall('brief_summary'):
+            textblock = data.find('textblock').text
+            clinicalSerachModel.brief_summary = textblock
+
+        # insert description textblock
+        for data in root.findall('detailed_description'):
+            clinicalSerachModel.brief_summary = data.find('textblock').text
+
+        clinicalSerachModel.save()
+
+        #insert primary outcomes and secondary outcome
+        c = get_object_or_404(ClinicalStudy, pk=clinicalSerachModel.pk)
+        for data in root.findall('primary_outcome'):
+            c.outcome_set.create(outcome_type="primary_outcome", measure=data.find('measure').text, timeFrame=data.find('time_frame').text, description=data.find('description').text)
+
+        for data in root.findall('secondary_outcome'):
+            c.outcome_set.create(outcome_type="secondary_outcome", measure=data.find('measure').text, timeFrame=data.find('time_frame').text, description=data.find('description').text)
+
+        # insert primary outcomes and secondary outcome
+        for data in root.findall('condition'):
+            c.condition_set.create(condition_name=data.text)
+
+        context = {}
         return HttpResponse(template.render(context, request))
